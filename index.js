a6831c9c713bba2e4a8f48dad5012eb87b080cb6
Daniel <vermont.daniel.johnson@gmail.com>
2020-02-06 14:22:18 -0500

    need function for random integer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
@@ -6,15 +6,22 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+// Creates a random integer between two numbers
+function randomInteger(min, max) {
+  let range = max - min + 1;
+  return min + Math.floor(Math.random() * range);
+}
 
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
+  console.log(
+    "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+  );
+  let secretNumber = await ask(
+    "What is your secret number?\nI won't peek, I promise...\n"
+  );
+  console.log("You entered: " + secretNumber);
   // Now try and complete the program.
   process.exit();
 }
-
-
