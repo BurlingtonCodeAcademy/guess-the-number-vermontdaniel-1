3b91ff465b9c9a7d02afc3847204e9a623f1b2a6
Daniel <vermont.daniel.johnson@gmail.com>
2020-02-09 21:11:50 -0500

    can guess your number

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 const correctAnswer = "yes" || "y";
 const wrongAnswer = "no" || "n";
 const highAnswer = "higher" || "h";
-const lowAnswer = "lower" || "l";
 
 // Functions --------------------------------
 function ask(questionText) {
@@ -27,32 +26,35 @@ start();
 
 async function start() {
   console.log(
-    "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+    "\nLet's play a game where you (human) make up a number\nand I (computer) try to guess it.\n"
   );
   // Game setup
   let setRangeMin = await ask("What is the minimum number I can guess to? ");
   setRangeMin = +setRangeMin;
   let setRangeMax = await ask("What is the maximum number I can guess to? ");
   setRangeMax = +setRangeMax;
-  let secretNumber = await ask("What is your secret number? ");
+  const secretNumber = await ask("What is your secret number? ");
   let computerGuess = randomInteger(setRangeMin, setRangeMax);
-  let guessQuestion = await ask(`Is your number ${computerGuess}? `);
+  let guessQuestion = null;
 
-  while (guessQuestion !== sanitizeString(correctAnswer))
-
-  if (guessQuestion === sanitizeString(wrongAnswer)) {
-    let highOrLow = await ask("Is it higher or lower? ");
-
-    if (highOrLow === sanitizeString(highAnswer)) {
-      console.log("You need to guess higher.");
-      setRangeMin = computerGuess;
-      guessQuestion = await ask(`Is your number ${computerGuess}? `);
-    } else {
-      console.log("You need to guess lower.");
-      setRangeMax = computerGuess;
-      guessQuestion = await ask(`Is your number ${computerGuess}? `);
+  while (guessQuestion !== correctAnswer) {
+    guessQuestion = await ask(`Is your number ${computerGuess}? `);
+    
+    if (guessQuestion === sanitizeString(wrongAnswer)) {
+      let highOrLow = await ask("Is it higher or lower? ");
+      // Start HighOrLow if loop branch
+      if (highOrLow === sanitizeString(highAnswer)) {
+        console.log("Looks like I need to guess higher.");
+        setRangeMin = computerGuess;
+        computerGuess = randomInteger(setRangeMin, setRangeMax);
+      } else {
+        console.log("Looks like I need to guess lower.");
+        setRangeMax = computerGuess;
+        computerGuess = randomInteger(setRangeMin, setRangeMax);
+      }
+      // End HighOrLow if loop branch
     }
-  }  
+  }
   console.log("YES! ARE YOU A PYSCHIC? HOW DID YOU GUESS MY NUMBER?");
   process.exit();
 }
